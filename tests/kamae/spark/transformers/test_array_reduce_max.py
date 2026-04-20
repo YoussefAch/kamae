fab084f04c692ec8648978982d6e05013acebe2f
Youssef Achenchabe <youssef.achenchabe@gmail.com>
2026-04-20 15:50:16 +0100

    Create test_array_reduce_max.py

---
+++ tests/kamae/spark/transformers/test_array_reduce_max.py
@@ -0,0 +1,71 @@
+# Copyright [2024] Expedia, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+import pytest
+from pyspark.sql.types import StructType, StructField, ArrayType, FloatType
+from kamae.spark.transformers import ArrayReduceMaxTransformer
+
+
+class TestArrayReduceMaxTransformer:
+    @pytest.fixture(scope="class")
+    def input_df(self, spark_session):
+        return spark_session.createDataFrame(
+            [
+                ([3.0, 1.0, 2.0],),
+                ([0.0, 5.0, 4.0],),
+                ([-3.0, -1.0, -2.0],),
+            ],
+            ["values"],
+        )
+
+    def test_returns_maximum_of_each_row(self, input_df):
+        transformer = ArrayReduceMaxTransformer(inputCol="values", outputCol="result")
+        result = transformer.transform(input_df).select("result").collect()
+
+        assert [row.result for row in result] == pytest.approx([3.0, 5.0, -1.0])
+
+    def test_default_value_for_empty_array(self, spark_session):
+
+        schema = StructType([
+            StructField("values", ArrayType(FloatType()), nullable=True)
+        ])
+
+        df = spark_session.createDataFrame(
+            [([],)],
+            schema=schema
+        )
+
+        transformer = ArrayReduceMaxTransformer(
+            inputCol="values", outputCol="result", defaultValue=-99.0
+        )
+        result = transformer.transform(df).select("result").collect()
+
+        assert result[0].result == pytest.approx(-99.0)
+
+    def test_spark_keras_parity(self, input_df):
+        import tensorflow as tf
+
+        transformer = ArrayReduceMaxTransformer(inputCol="values", outputCol="result")
+        spark_values = (
+            transformer.transform(input_df)
+            .select("result")
+            .rdd.map(lambda r: r[0])
+            .collect()
+        )
+
+        inputs = tf.constant([[3.0, 1.0, 2.0], [0.0, 5.0, 4.0], [-3.0, -1.0, -2.0]])
+        keras_values = transformer.get_tf_layer()(inputs).numpy().tolist()
+
+        np.testing.assert_array_almost_equal(spark_values, keras_values)
